050a80735e809561694e0dc76ab0a35f92838d5a
Cowation <cow2cow4cow@gmail.com>
2020-02-18 20:37:14 -0500

    Document Java files

--- src/main/java/me/derpee/creelande/Creelande.java
+++ src/main/java/me/derpee/creelande/Creelande.java
@@ -21,7 +21,8 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
+// The value here should match an entry in the META-INF/mods.toml file.
+// This should be "creelande", if not, refactor.
 @Mod("creelande")
 public class Creelande
 {
@@ -29,13 +30,13 @@ public class Creelande
     private static final Logger LOGGER = LogManager.getLogger();
 
     public Creelande() {
-        // Register the setup method for modloading
+        // Register the setup method for modloading.
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
+        // Register the enqueueIMC method for modloading.
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
+        // Register the processIMC method for modloading.
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-        // Register the doClientStuff method for modloading
+        // Register the doClientStuff method for modloading.
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
         // Register ourselves for server and other game events we are interested in
@@ -44,33 +45,36 @@ public class Creelande
 
     private void setup(final FMLCommonSetupEvent event)
     {
-        // some preinit code
+        // Run on Preinit (Red loading bar on startup)
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
-        // do something that can only be done on the client
+        // Run code that can only be ran on client.
         LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
     }
 
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
-        // some example code to dispatch IMC to another mod
+        // Dispath IMC to separate mod, use sendTo to send to a modId and run a method.
         InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
     {
-        // some example code to receive and process InterModComms from other mods
+        // Receiving and process incoming IMC dispatches.
         LOGGER.info("Got IMC {}", event.getIMCStream().
                 map(m->m.getMessageSupplier().get()).
                 collect(Collectors.toList()));
     }
+
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
+        // As server starts, run.
+        // Context: A Forge server can be deployed with its own mods folder.
+        // The mod will run this when it's ran from a server environment.
         LOGGER.info("HELLO from server starting");
     }
 
@@ -80,27 +84,23 @@ public class Creelande
     @ObjectHolder("creelande")
 
     public static class ModItems {
-        public static final Item phone = null;
-        //public static final IArmorMaterial supreme_armor = new IArmorMaterial("supreme", 4, new int[]{1, 1, 1, 1}, 6, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F);
-
+        // Set registerItems to register new Items upon call during init.
         @SubscribeEvent
         public static void registerItems(RegistryEvent.Register<Item> event) {
             event.getRegistry().registerAll(
-                    //new Item(new Item.Properties().maxStackSize(1).group(ItemGroup.REDSTONE)).setRegistryName("creelande", "phone")
+                    // Register new Items.
                     new Phone("phone", 1, ItemGroup.REDSTONE),
                     new Daddys_Credit_Card("daddys_credit_card", 1, ItemGroup.TOOLS),
                     new Superior_Armor(ArmorMaterialList.SUPERIOR, EquipmentSlotType.HEAD, "clout_goggles", ItemGroup.COMBAT),
                     new Superior_Armor(ArmorMaterialList.SUPERIOR, EquipmentSlotType.CHEST, "superior_chestplate", ItemGroup.COMBAT),
                     new Superior_Armor(ArmorMaterialList.SUPERIOR, EquipmentSlotType.LEGS, "adidas_pants", ItemGroup.COMBAT),
                     new Superior_Armor(ArmorMaterialList.SUPERIOR, EquipmentSlotType.FEET, "yeezys", ItemGroup.COMBAT)
-                    //new ArmorItem(ArmorMaterialList.SUPERIOR, EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)).setRegistryName("supreme_chestplate")
             );
         }
     }
     public static class RegistryEvents {
         @SubscribeEvent
         public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
             LOGGER.info("HELLO from Register Block");
         }
 
--- src/main/java/me/derpee/creelande/Daddys_Credit_Card.java
+++ src/main/java/me/derpee/creelande/Daddys_Credit_Card.java
@@ -3,7 +3,11 @@ package me.derpee.creelande;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 
+// Initialize a new Item class.
 public class Daddys_Credit_Card extends Item {
+    // Accept registryName (creelande:registry_name)
+    // Accept maxStackSize (Default: 1)
+    // Accept group (Example: ItemGroup.CREATIVE)
     public Daddys_Credit_Card(String registryName, int MaxStackSize, ItemGroup group) {
         super(new Item.Properties().maxStackSize(MaxStackSize).group(group));
         this.setRegistryName("creelande", registryName);
